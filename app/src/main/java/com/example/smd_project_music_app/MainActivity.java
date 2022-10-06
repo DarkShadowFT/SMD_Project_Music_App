19cae34595bcec3289ec9f500f6282a1e9203dac
DarkShadowFT <41124884+DarkShadowFT@users.noreply.github.com>
2022-10-06 10:52:08 +0500

    Initial commit

---
+++ app/src/main/java/com/example/smd_project_music_app/MainActivity.java
@@ -0,0 +1,14 @@
+t package com.example.smd_project_music_app;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+		@Override
+		protected void onCreate(Bundle savedInstanceState) {
+				super.onCreate(savedInstanceState);
+				setContentView(R.layout.activity_main);
+		}
+}
